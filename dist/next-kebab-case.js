33a08170fc7f218ce8c7e019a8d25a3fcf92e2b4
afeiship <1290657123@qq.com>
2019-12-25 10:31:12 +0800

    fix: new build

--- dist/next-kebab-case.js
+++ dist/next-kebab-case.js
@@ -3,16 +3,22 @@
  * description: Kebab case for next.
  * url: https://github.com/afeiship/next-kebab-case
  * version: 1.0.0
- * date: 2019-12-17 00:00:38
+ * date: 2019-12-25 10:31:05
  * license: MIT
  */
 
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
+  var REDUCE_RE = /([a-z])([A-Z])/g;
+  var REPLACE_RE = /[-\s_+]+/g;
 
-  nx.kebabCase = function() {
-    //code goes here.
+  nx.kebabCase = function(inString) {
+    if (!inString) return;
+    return (inString + '')
+      .replace(REDUCE_RE, '$1-$2')
+      .replace(REPLACE_RE, '-')
+      .toLowerCase();
   };
 
   if (typeof module !== 'undefined' && module.exports) {
