208171fa888c317786617cdd076d108e98f68ee6
afeiship <1290657123@qq.com>
2023-05-25 21:28:07 +0800

    Release 1.0.4

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ nx.kebabCase = function (inString) {
   return (inString + '').replace(REDUCE_RE, '$1-$2').replace(REPLACE_RE, '-').toLowerCase();
 };
 
-if (typeof module !== 'undefined' && module.exports) {
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
   module.exports = nx.kebabCase;
 }
 
